12695ae6a6be5a8ff512670178fbf881d8f8ff70
Khurshedul51 <princtonkadus@gmail.com>
2026-01-01 17:19:11 +0600

    solution of two leetcode problems(ContainsDuplicate_217, ValidAnagram_242) added

---
+++ LeetcodeProblemsSolutions/ContainsDuplicate_217/Program.cs
@@ -0,0 +1,37 @@
+﻿// Problem Link: https://leetcode.com/problems/contains-duplicate/
+
+namespace ContainsDuplicate_217
+{
+    public class Solution
+    {
+        private static bool ContainsDuplicate(int[] nums)
+        {
+            var hashSet = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(hashSet.Contains(nums[i]))
+                    return true;
+                else
+                    hashSet.Add(nums[i]);
+            }
+
+            return false;
+        }
+        public static void Main(string[] args)
+        {
+            //var n = int.Parse(Console.ReadLine());
+
+            //var nums = new int[n];
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    nums[i] = int.Parse(Console.ReadLine());
+            //}
+
+            int[] nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+            Console.WriteLine(ContainsDuplicate(nums));
+        }
+    }
+}
---
+++ LeetcodeProblemsSolutions/ValidAnagram_242/Program.cs
@@ -0,0 +1,52 @@
+﻿// Problem Link: https://leetcode.com/problems/valid-anagram/description/
+
+namespace ValidAnagram_242
+{
+    public class Solution
+    {
+        private static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) 
+                return false;
+
+            var dictS = new Dictionary<char, int>();
+            var dictT = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(dictS.ContainsKey(s[i]))
+                    dictS[s[i]] = dictS[s[i]] + 1;
+                else dictS.Add(s[i], 1);
+
+                if (dictT.ContainsKey(t[i]))
+                    dictT[t[i]] = dictT[t[i]] + 1;
+                else dictT.Add(t[i], 1);
+            }
+
+            if (dictS.Count != dictT.Count)
+                return false;
+
+            foreach( var pair in dictS)
+            {
+                var key = pair.Key;
+                var value = pair.Value;
+
+                if (!dictT.ContainsKey(key))
+                {
+                    return false;
+                }
+                else if (dictT[key] != value)
+                    return false;
+            }
+
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            var s = "rat";
+            var t = "car";
+
+            Console.WriteLine(IsAnagram(s, t));
+        }
+    }
+}
